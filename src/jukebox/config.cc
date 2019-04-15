023b77df9a9b2c54b0f5aa5f305ea51b3cbb771f
Valentin B <valentin.be@protonmail.com>
2019-04-15 20:02:20 +0200

    Refactor loaad_config

--- src/jukebox/config.cc
+++ src/jukebox/config.cc
@@ -4,17 +4,7 @@ namespace jukebox {
 
 YAML::Node load_config(std::string path) {
   YAML::Node config = YAML::LoadFile(path);
-  // The configuration section for the bot user that interacts with Discord.
-  YAML::Node bot_config = config["bot"];
-
-  // These are critical parts of the core configuration that should be present
-  // at any time.
-  if (!bot_config["token"] || !bot_config["guild_id"] ||
-      !bot_config["user_id"] || !bot_config["session_id"] ||
-      !bot_config["endpoint"]) {
-    throw std::runtime_error(
-        "Missing configuration entries in the 'bot' section.");
-  }
+  ASSERT_CONFIGURATION_OK(config);
 
   return config;
 }
--- src/jukebox/config.h
+++ src/jukebox/config.h
@@ -8,6 +8,16 @@
 
 namespace jukebox {
 
+// Used to check that the critical configuration entries required by Jukebox are
+// being provided.
+#define ASSERT_CONFIGURATION_OK(config)                              \
+  if (!config["bot"]["token"] || !config["bot"]["guild_id"] ||      \
+      !config["bot"]["user_id"] || !config["bot"]["session_id"] ||  \
+      !config["bot"]["endpoint"] || !config["server"]["address"] || \
+      !config["server"]["port"])                                    \
+    throw std::runtime_error("Missing required configuration entries!");
+
+// Loads the configuration files and verifies all required options.
 YAML::Node load_config(std::string path);
 
 }  // namespace jukebox
